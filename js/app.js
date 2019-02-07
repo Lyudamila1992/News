0c3b5c7e232340a599a148b3d70d4eeb2a2ff033
Lyudamila <lyudamila1107@gmail.com>
2019-02-08 01:53:47 +0200

    homework

--- js/app.js
+++ js/app.js
@@ -1,31 +1,54 @@
 const newsService = new NewsService();
+const newsSearch = new NewsSearch();
 const newsUI = new NewsUI();
+const alert = new Alert();
 
 // UI Elements
 const form = document.forms['newsControlForm'];
 const countrySelect = form['country'];
 const categorySelect = form['category'];
+const search = form['search'];
 
+/** function onSelectChange
+* @desc displays news on the page depending on the selected parameters
+* @param (e) 
+* @returns {*}
+*/
 
 function onSelectChange(e) {
     const country = countrySelect.value;
-    const category = categorySelect.value;
-    
+    const category = categorySelect.value;    
     if (!country || !category) return console.log('Выберите страну и категорию');
-
     newsService.getTopHeadlinesNews((response) => {
         const { articles } = response;
-        console.log('clear');
-        console.time();
         newsUI.clearContainer();
-        console.timeEnd();
-        console.log('add');
-        console.time();
         articles.forEach((news) => newsUI.addNews(news));
-        console.timeEnd();
     }, category, country);
+}
+
+/** function searchOn
+* @desc displays news on the page depending on the letters entered
+* @param (e) 
+* @returns {*}
+*/
+
+function searchOn(e) {
+   const value = search.value;
+   alert.alertOff();  
+    if (value.length >= 3) {
+        newsSearch.getSearchNews((response) => {
+            const { articles } = response;
+            if (articles.length === 0) {
+                alert.alertOn();
+            }; 
+            newsUI.clearContainer();
+            articles.forEach((news) => newsUI.addNews(news));
+        }, value);
+    };    
 }
 
 // Event listeners
 countrySelect.addEventListener('change', onSelectChange);
 categorySelect.addEventListener('change', onSelectChange);
+search.addEventListener("input", searchOn);
+
--- js/newsService.js
+++ js/newsService.js
@@ -8,10 +8,42 @@ class NewsService {
         this.category = 'technology';
     }
 
-    /**
-     * Get all news
-     */
     getTopHeadlinesNews(callback, category = this.category, country = this.country) {
         http.get(`${this.apiUrl}/top-headlines?country=${country}&category=${category}&apiKey=${this.apiKey}`, callback);
+    }
+}
+
+class NewsSearch {
+    constructor() {
+        this.apiUrl = 'https://newsapi.org/v2';
+        this.apiKey = '9c27b0f722b84da5a08312d2b125351b';
+        this.value = '';
+    }
+
+    getSearchNews(callback, value = this.value) {
+        http.get(`${this.apiUrl}/everything?q=${value}&apiKey=${this.apiKey}`, callback);
+    }
+}
+
+class Alert {
+    constructor() {  
+    }
+
+    alertOn() {
+        const template = Alert.alertT();
+        let container = document.querySelector(".news-wrap");
+        container.insertAdjacentHTML('afterbegin', template);
+    }
+
+    static alertT() {
+        return `
+            <div class="card red darken-1 white-text" >Новости не найдены!</div>
+        `;
+    }
+
+    alertOff() {
+        const currentAlert = document.querySelector('.red');
+        let container = document.querySelector(".news-wrap");
+        if (currentAlert) container.removeChild(currentAlert);
     }
 }
